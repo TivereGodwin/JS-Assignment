cb55ad6ab66e264c3d39c5b620f029874e5cf684
TivereGodwin <77321565+TivereGodwin@users.noreply.github.com>
2025-03-01 16:56:11 +0100

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,20 @@
+//Declaring  Variables
+let num1 = 12
+let num2 = 17
+
+//Arithmetic Operations
+let Addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1/num2;
+let modulus = num1 % num2;
+
+console.log("Arithmetic Operations:");{
+//console.log('');
+console.log  ("Addition:" + num1 + "+" + num2 + '=' + Addition);
+console.log  ("subtraction:" + num1 + "-" + num2 + '=' + subtraction);
+console.log  ("multiplication:" + num1 + "*" + num2 + '=' + multiplication);
+console.log  ("division:" + num1 + "/" + num2 + '=' + divisionion);
+console.log  ("modulus:" + num1 + "%" + num2 + '=' + modulus);
+//console.log ('');
+}
